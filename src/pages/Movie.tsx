625d0f1be97c3901f9c18778b9df29e2e1d85934
Glitch (starter-cra-and-express) <none>
2023-09-22 21:27:27 +0800

    Movie details basic info

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const Movie = () => {
+const Movie: React.FC = () => {
+  const [movie, setMovie] = useState();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const { data } = await axios.get(`/api/movies/${id}`);
+        setMovie(data);
+      } catch (error) {
+        console.log('Movie not found');
+      }
+    };
+
+    getMovie();
+  }, [id]);
+
   return (
-    <div>Movie lover</div>
-  )
-}
+    <div className="px-page">
+      {console.log("Movie ", movie)}
+      <div className="px-top">
+        <h2 className="px-page-title">Movie Details</h2>
+      </div>
+      <div className="px-preview">
+        <span className="px-status">{movie?.status}</span>
+      </div>
+      <div className="px-mov-name">
+        <span className="px-tag-line">{movie?.tagline}</span>
+        <h3 className="px-mov-title">{movie?.title}</h3>
+        <span className="px-id">{movie?.id}</span>
+      </div>
+      <div className="px-mov-desc">{movie.overview}</div>
+    </div>
+  );
+};
 
-export default Movie
+export default Movie;
